4d5520a312d3da8d600abb98645cc9fd1fcec3ac
Dreanor <mikeundead@googlemail.com>
2024-05-14 22:08:02 +0200

    Update fflogs-patcher.ps1

--- fflogs-patcher.ps1
+++ fflogs-patcher.ps1
@@ -1,5 +1,5 @@
 $zipExecutable = "C:\Program Files\7-Zip\7z.exe"
-$fflogsJs = "$($env:LOCALAPPDATA)\Programs\FF Logs Uploader\resources\"
+$fflogsJs = "$($env:LOCALAPPDATA)\Programs\FF Logs Uploader\resources"
 
 & $zipExecutable x "$fflogsJs\app.asar" "js\app.js" -o"$fflogsJs"
 
@@ -8,6 +8,6 @@ $content = Get-Content -Path $appjs
 
 $content -replace "gh.showBottomRightAd&&!e\?.enabledFeatures.noAds", "false" -replace "showBottomRightAd=!0", "showBottomRightAd = false" -replace "showBottomRightAd=!1", "showBottomRightAd = false" | Set-Content -Path $appjs
 
-& $zipExecutable a "$fflogsJs\app.asar" "js\app.js"
+& $zipExecutable a "$fflogsJs\app.asar" "$fflogsJs\js\app.js"
 
 Start-Sleep -Milliseconds 2000
